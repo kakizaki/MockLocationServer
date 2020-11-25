09e2a865fb5cc46e7264647e4471a30a067effc4
kakizaki <kakizaki@tecraft.co.jp>
2020-11-25 13:53:25 +0900

    add Permission check

--- app/src/main/java/com/example/mocklocationserver/web/MainActivity.kt
+++ app/src/main/java/com/example/mocklocationserver/web/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.mocklocationserver.web
 
+import android.Manifest
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val REQUESTCODE_PERMISSION_LOCATION = 1
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +26,51 @@ class MainActivity : AppCompatActivity() {
 
         start_service.setOnClickListener { startWebServer() }
         stop_service.setOnClickListener { stopWebServer() }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+
+        checkLocationPermission()
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUESTCODE_PERMISSION_LOCATION) {
+            checkLocationPermission()
+        }
+    }
+
+
+    fun checkLocationPermission() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION))  {
+            // パーミッションの許可の際に "今後、表示しない" にチェックを付けた場合
+            val alert = AlertDialog.Builder(this)
+                .setTitle(R.string.permission_dialog_title)
+                .setMessage(R.string.permission_dialog_message)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }
+                .create()
+                .show()
+        }
+        else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), REQUESTCODE_PERMISSION_LOCATION)
+        }
     }
 
 
